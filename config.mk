a612e354070e306c250a2f2922d498d8281ad59f
Chou Yatingde <chouyatingde@gmail.com>
2012-10-07 19:42:30 +0200

    Turn off -Os
    For debug purpose no optimization.

--- config.mk
+++ config.mk
@@ -186,7 +186,7 @@ endif
 ARFLAGS = $(error update your Makefile to use cmd_link_o_target and not AR)
 RELFLAGS= $(PLATFORM_RELFLAGS)
 DBGFLAGS= -g # -DDEBUG
-OPTFLAGS= -Os #-fomit-frame-pointer
+OPTFLAGS= -O0 #-fomit-frame-pointer
 
 OBJCFLAGS += --gap-fill=0xff
 
