e85d03d73d248ee236390fa83954164cb7753c8e
Chou Yatingde <chouyatingde@gmail.com>
2012-10-07 22:17:24 +0200

    Fixup: Pad_to calculation in nand_spl

--- nand_spl/board/samsung/fl2440/config.mk
+++ nand_spl/board/samsung/fl2440/config.mk
@@ -33,8 +33,7 @@ include $(TOPDIR)/board/$(BOARDDIR)/config.mk
 
 # PAD_TO used to generate a 4kByte binary needed for the combined image
 # -> PAD_TO = CONFIG_SYS_TEXT_BASE + 4096
-PAD_TO	:= $(shell expr $$[$(CONFIG_SYS_TEXT_BASE) + 4096])
-
+PAD_TO	:= $(shell expr $(CONFIG_SYS_TEXT_BASE) + 4096)
 ifeq ($(debug),1)
 PLATFORM_CPPFLAGS += -DDEBUG
 endif
